5b0ae5912331d234091516ff0dcc998b3225c8c4
Bill Fero <billfero@gmail.com>
2017-07-17 13:12:27 +0000

    generate static home and help pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  # add routes for the home and help pages
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'application#hello'
