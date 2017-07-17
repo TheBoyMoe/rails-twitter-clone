02380a7c485f7d5d94adef9ce0281a129f0c4be7
Bill Fero <billfero@gmail.com>
2017-07-17 15:59:30 +0000

    impl about page and controller using tdd

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   
   # add routes for the home and help pages
   get 'static_pages/home'
-
   get 'static_pages/help'
+  get 'static_pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
