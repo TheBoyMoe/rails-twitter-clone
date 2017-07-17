eb33dd14da4b4ba0f6f86bfef38241e3239cca35
Bill Fero <billfero@gmail.com>
2017-07-17 17:50:36 +0000

    update root route to point to the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   
-  # add routes for the home and help pages
+  # routes for static pages
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root 'application#hello'
+  # root route - home page
+  root 'static_pages#home'
+  
 end
